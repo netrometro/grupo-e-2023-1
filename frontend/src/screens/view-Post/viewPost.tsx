d428ee4256d22e832b5b63a9e70620917a533f0d
Emílio <emiliogabriel000@gmail.com>
2023-08-27 03:35:28 -0300

    Filtro por tipo de serviço funcionando

--- frontend/src/screens/view-Post/viewPost.tsx
+++ frontend/src/screens/view-Post/viewPost.tsx
@@ -3,11 +3,14 @@ import { View, Text, FlatList, TextInput, Button, TouchableOpacity } from 'react
 import styles from './style';
 import api from '../../service/api'
 import Card from '../../components/Card/card';
+import { Picker } from '@react-native-picker/picker';
 
 const ListagemPostagensScreen = ({ route }: any) => {
     const [exibirPostagens, setExibirPostagens] = useState(false);
     const [minPrice, setMinPrice] = useState('');
     const [maxPrice, setMaxPrice] = useState('');
+    const [tiposDeServico, setTiposDeServico] = useState([]);
+    const [tipoServicoSelecionado, setTipoServicoSelecionado] = useState('');
 
     const priceFilter = () => {
       api.get('/postagens/filter-price', {
@@ -24,17 +27,42 @@ const ListagemPostagensScreen = ({ route }: any) => {
       });
     };
 
-      useEffect(() => {
-        api.get('/postagens')
-        .then(response =>{
-            setExibirPostagens(response.data)
-        })
-        .catch(error => {
-            console.error('Erro', error);
-        
-        })
-    },[]);
+    const getPostagens = async () => {
+      try {
+        const response = await api.get('/postagens');
+        setExibirPostagens(response.data);
+      } catch (error) {
+        console.error('Erro', error);
+      }
+    };
 
+    const getTiposDeServico = async () => {
+      try {
+        const response = await api.get('/allService');
+        setTiposDeServico(response.data);
+      } catch (error) {
+        console.error('Erro ao carregar tipos de serviço:', error);
+      }
+    };
+
+    const tipoServicoFilter = () => {
+      api.get(`/typeService/${tipoServicoSelecionado}`, {
+        params: {
+          tipoServicoId: tipoServicoSelecionado
+        }
+      })
+      .then(response => {
+        setExibirPostagens(response.data);
+      })
+      .catch(error => {
+        console.error('Erro', error);
+      });
+    };
+
+    useEffect(() => {
+      getPostagens();
+      getTiposDeServico();
+    }, []);
 
   return (
     <View style={styles.container}>
@@ -53,6 +81,17 @@ const ListagemPostagensScreen = ({ route }: any) => {
         style={[styles.input, styles.inputWhiteBackground]}
 
       />
+
+      <Picker
+        style={[styles.input, styles.inputWhiteBackground]}
+        selectedValue={tipoServicoSelecionado}
+        onValueChange={(itemValue) => setTipoServicoSelecionado(itemValue)}
+      >
+        <Picker.Item label="Selecione um tipo de serviço" value="" />
+        {tiposDeServico.map((tipo) => (
+          <Picker.Item key={tipo.id} label={tipo.nomeServico} value={tipo.id.toString()} />
+        ))}
+      </Picker>
         <View style={styles.buttonContainer}>
         <Button
         
@@ -60,7 +99,18 @@ const ListagemPostagensScreen = ({ route }: any) => {
           onPress={priceFilter}
 
         />
+      
       </View>
+      <View style={styles.buttonContainer}>
+      <Button
+        title="Filtrar por tipo de serviço"
+        onPress={tipoServicoFilter}
+      />
+      
+      </View>
+
+      
+     
       <FlatList
         data={exibirPostagens}
         keyExtractor={item => item.id.toString()}
