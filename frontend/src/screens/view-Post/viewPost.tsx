ee0a2fb402fa10570d0d2381a2a38269e675c6a2
Emílio <emiliogabriel000@gmail.com>
2023-08-27 02:57:17 -0300

    Filtro de preço funcional no front

--- frontend/src/screens/view-Post/viewPost.tsx
+++ frontend/src/screens/view-Post/viewPost.tsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
 import styles from './style';
 import api from '../../service/api'
 import Card from '../../components/Card/card';
 
 const ListagemPostagensScreen = ({ route }: any) => {
     const [exibirPostagens, setExibirPostagens] = useState(false);
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
+
+    const priceFilter = () => {
+      api.get('/postagens/filter-price', {
+        params: {
+          minPrice: minPrice !== '' ? parseFloat(minPrice) : undefined,
+          maxPrice: maxPrice !== '' ? parseFloat(maxPrice) : undefined
+        }
+      })
+      .then(response => {
+        setExibirPostagens(response.data);
+      })
+      .catch(error => {
+        console.error('Erro', error);
+      });
+    };
 
       useEffect(() => {
         api.get('/postagens')
@@ -22,6 +39,28 @@ const ListagemPostagensScreen = ({ route }: any) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Postagens</Text>
+      <TextInput
+        placeholder="Preço mínimo"
+        value={minPrice}
+        onChangeText={setMinPrice}
+        style={[styles.input, styles.inputWhiteBackground]}
+
+      />
+      <TextInput
+        placeholder="Preço máximo"
+        value={maxPrice}
+        onChangeText={setMaxPrice}
+        style={[styles.input, styles.inputWhiteBackground]}
+
+      />
+        <View style={styles.buttonContainer}>
+        <Button
+        
+          title="Filtrar por preço"
+          onPress={priceFilter}
+
+        />
+      </View>
       <FlatList
         data={exibirPostagens}
         keyExtractor={item => item.id.toString()}
