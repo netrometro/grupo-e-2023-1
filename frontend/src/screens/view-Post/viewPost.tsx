c83ac8dbaceb9dc9aa78e2f237ed7ef08aece64b
Emílio <emiliogabriel000@gmail.com>
2023-08-29 03:41:06 -0300

    Refatoração de filtros e adicionando filtro de contrato ou sem contrato

--- frontend/src/screens/view-Post/viewPost.tsx
+++ frontend/src/screens/view-Post/viewPost.tsx
@@ -1,68 +1,72 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TextInput, Button } from 'react-native';
 import styles from './style';
-import api from '../../service/api'
+import api from '../../service/api';
 import Card from '../../components/Card/card';
 import { Picker } from '@react-native-picker/picker';
 
 const ListagemPostagensScreen = ({ route }: any) => {
-    const [exibirPostagens, setExibirPostagens] = useState(false);
-    const [minPrice, setMinPrice] = useState('');
-    const [maxPrice, setMaxPrice] = useState('');
-    const [tiposDeServico, setTiposDeServico] = useState([]);
-    const [tipoServicoSelecionado, setTipoServicoSelecionado] = useState('');
+  const [exibirPostagens, setExibirPostagens] = useState([]);
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [tiposDeServico, setTiposDeServico] = useState([]);
+  const [tipoServicoSelecionado, setTipoServicoSelecionado] = useState('');
+  const [tipoContratoSelecionado, setTipoContratoSelecionado] = useState('');
+  const [filtroModificado, setFiltroModificado] = useState(false);
 
-    const priceFilter = () => {
-      api.get('/postagens/filter-price', {
+
+  const getPostagens = async () => {
+    try {
+      const response = await api.get('/postagens');
+      setExibirPostagens(response.data);
+    } catch (error) {
+      console.error('Erro', error);
+    }
+  };
+
+  const applyFilters = async () => {
+    let response;
+    
+    if (minPrice !== '' || maxPrice !== '') {
+      response = await api.get('/postagens/filter-price', {
         params: {
           minPrice: minPrice !== '' ? parseFloat(minPrice) : undefined,
-          maxPrice: maxPrice !== '' ? parseFloat(maxPrice) : undefined
-        }
-      })
-      .then(response => {
-        setExibirPostagens(response.data);
-      })
-      .catch(error => {
-        console.error('Erro', error);
+          maxPrice: maxPrice !== '' ? parseFloat(maxPrice) : undefined,
+        },
       });
-    };
-
-    const getPostagens = async () => {
-      try {
-        const response = await api.get('/postagens');
-        setExibirPostagens(response.data);
-      } catch (error) {
-        console.error('Erro', error);
-      }
-    };
-
-    const getTiposDeServico = async () => {
-      try {
-        const response = await api.get('/allService');
-        setTiposDeServico(response.data);
-      } catch (error) {
-        console.error('Erro ao carregar tipos de serviço:', error);
-      }
-    };
-
-    const tipoServicoFilter = () => {
-      api.get(`/typeService/${tipoServicoSelecionado}`, {
+    } else if (tipoContratoSelecionado === 'com') {
+      response = await api.get('/postagens/comContrato');
+    } else if (tipoContratoSelecionado === 'sem') {
+      response = await api.get('/postagens/semContrato');
+    } else if (tipoServicoSelecionado) {
+      response = await api.get(`/typeService/${tipoServicoSelecionado}`, {
         params: {
-          tipoServicoId: tipoServicoSelecionado
-        }
-      })
-      .then(response => {
-        setExibirPostagens(response.data);
-      })
-      .catch(error => {
-        console.error('Erro', error);
+          tipoServicoId: tipoServicoSelecionado,
+        },
       });
-    };
+    } else {
+      response = await getPostagens(); 
+    }
+    
+    console.log('Response data:', response.data);
+    setExibirPostagens(response.data);
+  };
+  
 
-    useEffect(() => {
-      getPostagens();
-      getTiposDeServico();
-    }, []);
+  const getTiposDeServico = async () => {
+    try {
+      const response = await api.get('/allService');
+      setTiposDeServico(response.data);
+    } catch (error) {
+      console.error('Erro ao carregar tipos de serviço:', error);
+    }
+  };
+
+  useEffect(() => {
+    getTiposDeServico();
+    getPostagens()
+    
+  }, [ minPrice, maxPrice]);
 
   return (
     <View style={styles.container}>
@@ -72,45 +76,45 @@ const ListagemPostagensScreen = ({ route }: any) => {
         value={minPrice}
         onChangeText={setMinPrice}
         style={[styles.input, styles.inputWhiteBackground]}
-
       />
       <TextInput
         placeholder="Preço máximo"
         value={maxPrice}
         onChangeText={setMaxPrice}
         style={[styles.input, styles.inputWhiteBackground]}
-
       />
 
       <Picker
         style={[styles.input, styles.inputWhiteBackground]}
         selectedValue={tipoServicoSelecionado}
-        onValueChange={(itemValue) => setTipoServicoSelecionado(itemValue)}
+        onValueChange={itemValue => setTipoServicoSelecionado(itemValue)}
+        
       >
         <Picker.Item label="Selecione um tipo de serviço" value="" />
-        {tiposDeServico.map((tipo) => (
-          <Picker.Item key={tipo.id} label={tipo.nomeServico} value={tipo.id.toString()} />
+        {tiposDeServico.map(tipo => (
+          <Picker.Item
+            key={tipo.id}
+            label={tipo.nomeServico}
+            value={tipo.id.toString()}
+          />
         ))}
       </Picker>
-        <View style={styles.buttonContainer}>
-        <Button
-        
-          title="Filtrar por preço"
-          onPress={priceFilter}
 
-        />
-      
-      </View>
+      <Picker
+        style={[styles.input, styles.inputWhiteBackground]}
+        selectedValue={tipoContratoSelecionado}
+        onValueChange={itemValue => setTipoContratoSelecionado(itemValue)}
+      >
+        <Picker.Item label="Selecione um tipo de contrato" value="" />
+        <Picker.Item label="Com contrato" value="com" />
+        <Picker.Item label="Sem contrato" value="sem" />
+        <Picker.Item label="Com todos os contratos" value="todos" />
+      </Picker>
+
       <View style={styles.buttonContainer}>
-      <Button
-        title="Filtrar por tipo de serviço"
-        onPress={tipoServicoFilter}
-      />
-      
+        <Button title="Filtrar" onPress={applyFilters} />
       </View>
 
-      
-     
       <FlatList
         data={exibirPostagens}
         keyExtractor={item => item.id.toString()}
